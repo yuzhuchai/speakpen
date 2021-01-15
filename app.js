cecf3796cdadde9cf47833ec87bb7a86303adf71
yuzhu chai <yzhuchai@gmail.com>
2021-01-15 17:20:29 +0800

    add constrain to all divs when resizing, simplifying code

--- app.js
+++ app.js
@@ -15,22 +15,11 @@ function resizeY(e){
     const dy = y_pos - e.y;
     y_pos = e.y;
     let consoleHeight = parseInt(getComputedStyle(consoleDiv, '').height)
-    if(consoleHeight >= 60 && consoleHeight <= 500){
+    if((consoleHeight >= 60 && consoleHeight <= 400) || (consoleHeight < 60 && dy >= 0) || (consoleHeight > 400 && dy <= 0)){
         consoleDiv.style.height = (consoleHeight + dy) + "px";
         containerDiv.style.height = (parseInt(getComputedStyle(containerDiv, '').height) - dy) + "px";
         console.log(dy)
-    } else if (consoleHeight < 60 && dy > 0){
-        consoleDiv.style.height = (consoleHeight + dy) + "px";
-        containerDiv.style.height = (parseInt(getComputedStyle(containerDiv, '').height) - dy) + "px";
-        console.log(dy)
-    } else if (consoleHeight > 500 && dy < 0){
-        consoleDiv.style.height = (consoleHeight + dy) + "px";
-        containerDiv.style.height = (parseInt(getComputedStyle(containerDiv, '').height) - dy) + "px";
-        console.log(dy)
-    }
-//   console.log(consoleDiv.style.height);
-  console.log(consoleHeight)
-//   console.log('hello'+y_pos)
+    } 
 }
 
 consoleDiv.addEventListener("mousedown", function(e){
@@ -40,14 +29,17 @@ consoleDiv.addEventListener("mousedown", function(e){
   }
 }, false);
 
+
 function resizeXRight(e){
     const dx = x_pos - e.x;
     x_pos = e.x;
-    extraDiv.style.width = (parseInt(getComputedStyle(extraDiv, '').width) + dx) + "px";
-    canvasDiv.style.width = (parseInt(getComputedStyle(canvasDiv, '').width) - dx) + "px";
-    // console.log('hello'+x_pos)
+    let extraWidth = parseInt(getComputedStyle(extraDiv, '').width)
+  if((extraWidth >= 60 && extraWidth <=400) || (extraWidth < 60 && dx >= 0) || (extraWidth > 400 && dx <=0)){
+        extraDiv.style.width = (extraWidth + dx) + "px";
+        canvasDiv.style.width = (parseInt(getComputedStyle(canvasDiv, '').width) - dx) + "px";
+        console.log(extraWidth, dx)
+    }
 }
-
 
 extraDiv.addEventListener('mousedown', function(e){
     if(e.offsetX < BORDER_SIZE){
@@ -59,12 +51,14 @@ extraDiv.addEventListener('mousedown', function(e){
 function resizeXLeft(e){
     const dx = x_pos - e.x;
     x_pos = e.x;
-    toolsDiv.style.width = (parseInt(getComputedStyle(toolsDiv, '').width) - dx) + "px";
-    canvasDiv.style.width = (parseInt(getComputedStyle(canvasDiv, '').width) + dx) + "px";
-    canvasDiv.style.left = (parseInt(getComputedStyle(canvasDiv, '').left) - dx) + "px";
-    // console.log('hello'+x_pos)
+    let toolsWidth = parseInt(getComputedStyle(toolsDiv, '').width)
+    if((toolsWidth >=60 && toolsWidth <= 400) || (toolsWidth < 60 && dx <= 0) || (toolsWidth > 400 && dx >= 0)){
+        toolsDiv.style.width = (toolsWidth - dx) + "px";
+        canvasDiv.style.width = (parseInt(getComputedStyle(canvasDiv, '').width) + dx) + "px";
+        canvasDiv.style.left = (parseInt(getComputedStyle(canvasDiv, '').left) - dx) + "px";
+        // console.log('hello'+x_pos)
+    }
 }
-
 
 toolsDiv.addEventListener('mousedown', function(e){
     if(e.offsetX > BORDER_SIZE){
